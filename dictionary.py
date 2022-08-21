78d691305842e3ba55fb1621aa2ba0dde3a53078
BuildTools <woko@atlas.cz>
2022-08-21 21:01:28 +0200

    improved

--- dictionary.py
+++ dictionary.py
@@ -1,24 +1,12 @@
-class Dict:
+class Dict(dict):
     def __init__(self):
-        self.dict = dict()
+        super(Dict, self).__init__()
 
     def add_item(self, key: str, value: int):
-        if key in self.dict.keys():
-            self.dict[key] += value
-        self.dict.setdefault(key, value)
+        if key in self.keys():
+            self.update({key: self.get(key) + value})
+        self.setdefault(key, value)
 
     def remove_item(self, key: str):
-        if key in self.dict.keys():
-            self.dict.__delitem__(key)
-
-    def clear(self):
-        self.dict.clear()
-
-
-# if __name__ == '__main__':
-    # d = Dict()
-    # d.add_item("mates", 10)
-    # print(d.dict)
-    # d.remove_item("mates")
-    # print(d.dict)
-
+        if key in self.keys():
+            self.__delitem__(key)
