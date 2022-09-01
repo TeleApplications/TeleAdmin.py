7b7efe877add58285214132432bdb79992878462
BuildTools <woko@atlas.cz>
2022-09-01 18:20:18 +0200

    improved imports and added timer for refreshing window

--- Application/Widgets/displayproducts_window.py
+++ Application/Widgets/displayproducts_window.py
@@ -1,4 +1,4 @@
-from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtCore import Qt, QPoint, QTimer
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel, QFrame
 
@@ -32,12 +32,17 @@ class DisplayProductsWindow(QWidget):
         self.oldPostition = QPoint()
         self.mainLayout = QGridLayout()
         self.dictionary = Dictionary()
+        self.timer = QTimer()
+        self.timer.start(10000)
+
+
+        self.timer.timeout.connect(self.loadData)
         self.setLayout(self.mainLayout)
+
+    def loadData(self):
 
         self.getThread = DatabaseThread(data.get("DPWloadDictionary"), self.__init_dict)
         self.getThread2 = DatabaseThread(data.get("DPWloadData"), self.__content)
-
-    def loadData(self):
         self.getThread.run()
         self.getThread2.run()
 
@@ -59,6 +64,7 @@ class DisplayProductsWindow(QWidget):
                 if items_number > 0:
                     items_number -= 1
                     frame = QFrame()
+                    frame.setObjectName("frame")
                     layout = QGridLayout()
 
                     item = Item(*items[x * width + y])
@@ -67,14 +73,28 @@ class DisplayProductsWindow(QWidget):
                     except (KeyError, FileNotFoundError):
                         pixmap.loadFromData(self.__open_image(PATH + "Unknown_Image.jpg"))
 
-                    label = QLabel()
+                    image_label = QLabel()
+                    image_label.setContentsMargins(0, 0, 0, 0)
+                    amount_label = QLabel(item.amount)
+                    amount_label.setFixedSize(150, 25)
+                    if int(item.amount.split(" ")[0]) <= 0:
+                        amount_label.setText("Vyprodáno")
+                        amount_label.setStyleSheet("background-color: red; color:white;")
 
                     scaled_pixmap = pixmap.scaled(128, 128, Qt.KeepAspectRatio)
-                    label.setPixmap(scaled_pixmap)
-                    layout.addWidget(label, 0, 0, 1, 2)
-                    layout.addWidget(QLabel(item.name), 1, 0, 1, 2)
-                    layout.addWidget(QLabel(item.price), 2, 0)
-                    layout.addWidget(QLabel(item.amount), 3, 0)
+                    image_label.setPixmap(scaled_pixmap)
+
+                    name_label = QLabel(item.name)
+                    name_label.setFixedWidth(150)
+                    name_label.setWordWrap(True)
+
+                    price_label = QLabel(item.price)
+                    price_label.setFixedSize(150, 20)
+
+                    layout.addWidget(image_label, 0, 0)
+                    layout.addWidget(name_label, 1, 0)
+                    layout.addWidget(price_label, 3, 0)
+                    layout.addWidget(amount_label, 2, 0)
 
                     frame.setLayout(layout)
                     self.mainLayout.addWidget(frame, x, y)
